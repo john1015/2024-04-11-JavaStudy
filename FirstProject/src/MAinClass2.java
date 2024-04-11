f151349c30f97226fafc64b3d138813020163944
sist114_1 <sist114_1@sist114>
2024-04-11 17:43:27 +0900

    1111

--- FirstProject/src/MAinClass2.java
+++ FirstProject/src/MAinClass2.java
@@ -33,11 +33,38 @@
  	  문자열표현법 => "Hello" -> 여러 문자 사용 가능
  
  */
+
 public class MAinClass2 {
 
 	public static void main(String[] args) {
-		System.out.println("\"홍길동\""); // "홍길동"으로 출력하고 싶을때 => 명령문 종료할때 반드시 ; 붙이기
-		System.out.println("c:\\javaDev\\movie.txt"); //경로명 같은경우는 이미 사용하기 때문에 2번쓰기
+		// System.out.println("\"홍길동\""); // "홍길동"으로 출력하고 싶을때 => 명령문 종료할때 반드시 ; 붙이기
+		// System.out.println("c:\\javaDev\\movie.txt"); //경로명 같은경우는 이미 사용하기 때문에 2번쓰기
+		// 홍길동  남자 로 출력하고싶을때 -> System.out.println("홍길동\t남자");
+		// 홍길동
+		// 남자 이렇게하고싶으면 \n
+		// 홍길동 남자 26 print 3번 아니면 각각 + 
+		//System.out.println("홍길동\t"+"남자\t"+"26");
+		/*
+		   *
+		   **
+		   ***
+		   ****
+		   ***** 
+		 	★ -> ㅁ+한자
+		 */
+		
+		System.out.println("★");
+		System.out.println("★★");
+		System.out.println("★★★");
+		System.out.println("★★★★");
+		System.out.println("★★★★★");
+		
+		for(int i=0; i<=5; i++) {
+			for(int j=0; j<=5-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
