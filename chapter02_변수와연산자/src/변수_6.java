91ebf0f652b2b924e161ca9b238bfe318997f87a
정유엽 <39753330+john1015@users.noreply.github.com>
2024-04-12 17:07:46 +0900

    Add files via upload

--- chapter02_변수와연산자/src/변수_6.java
+++ chapter02_변수와연산자/src/변수_6.java
@@ -12,7 +12,30 @@ public class 변수_6 {
 		//4byte 소수점은 6자리까지
 		double doubleValue = 10.5;
 		//실수의 디폴트	
-		//정수 
+		//정수 → int , 실수 → double
+		//실제 프로그램에서는  int,double 가장 많이 사용된다
+		// 변수 → 계속 상태가 변경 ... / 고정 → 상수
+		boolean booleanVlaue = true;
+		//→ 조건식 1byte  →저장은 true/false만 저장이 가능
+		char charValue='A';
+		//문자 1개 저장 공간 → 2byte(0~65535)
+		/*
+		 *  byte = **char = int = long = double
+		 *  boolean   ↓
+		 *          String
+		 */
+		char c1 = '된';
+		char c2 = '장';
+		char c3 = '국';
+		char c4 = ' ';
+		char c5 = '끓';
+		char c6 = '이';
+		char c7 = '는';
+		char c8 = ' ';
+		char c9 = '법';
+		System.out.println(c1+""+c2+c3+c4+c5+c6+c7+c8+c9);
+		String s = "된장국 끓이는 법";
+		System.out.println(s);
 	}
    
 }
