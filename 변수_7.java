7bd09fd044e2b9d6a8bcd509de4c11d4778ab974
정유엽 <39753330+john1015@users.noreply.github.com>
2024-04-12 17:06:16 +0900

    0412

---
+++ 변수_7.java
@@ -0,0 +1,17 @@
+/*
+ * 값을 교환
+ */
+public class 변수_7 {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=200;
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println("a : "+a);
+		System.out.println("b : "+b);
+		   
+	}
+
+}
