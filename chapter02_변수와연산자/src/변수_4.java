1b9824d5b09ff972d0ca62e0b29ea96d9c6c48b1
정유엽 <39753330+john1015@users.noreply.github.com>
2024-04-12 15:54:34 +0900

    Add files via upload

--- chapter02_변수와연산자/src/변수_4.java
+++ chapter02_변수와연산자/src/변수_4.java
@@ -9,6 +9,16 @@ public class 변수_4 {
 
 	public static void main(String[] args) {
 		
+		int m = 5600;
+		int lunch = 3000;
+		int coffee = 1500;
+		int fun = 250;
+		
+		System.out.println("돈 : "+m);
+		System.out.println("점심 : "+lunch);
+		System.out.println("커피 : "+coffee);
+		System.out.println("심심풀이 : "+fun);
+		System.out.println("남은돈 : "+m+" - "+lunch+" - "+coffee+" - "+fun+" = "+(m-lunch-coffee-fun));
 		
 	}
 
