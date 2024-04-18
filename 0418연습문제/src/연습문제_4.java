db164c2af1bdc857b1f7b3682f960bd24dc3faca
정유엽 <39753330+john1015@users.noreply.github.com>
2024-04-18 17:42:42 +0900

    0418

--- 0418연습문제/src/연습문제_4.java
+++ 0418연습문제/src/연습문제_4.java
@@ -4,7 +4,7 @@ public class 연습문제_4 {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
 			if(i==10) System.out.println(i*5);
-			else System.out.print(i*5 +" , ");
+			else System.out.printf("%d\t",i*5);
 		}
 	}
 
--- 0418연습문제/src/연습문제_5.java
+++ 0418연습문제/src/연습문제_5.java
@@ -3,8 +3,8 @@ public class 연습문제_5 {
 	public static void main(String[] args) {
 		for (char i = 'A'; i <= 'N'; i++) {
 			if(i%2==0) {
-				System.out.print(i+" , ");
-				if(i=='N') System.out.println(i);
+				System.out.printf("%c\t",i);
+				if(i=='N') System.out.printf("%c",i);
 			}
 			
 		}
