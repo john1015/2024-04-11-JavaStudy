920ee9a30f1893871f14fafb07ac7f4dd0a11a5f
정유엽 <39753330+john1015@users.noreply.github.com>
2024-04-18 17:14:43 +0900

    0418

--- 0418연습문제/src/연습문제_1.java
+++ 0418연습문제/src/연습문제_1.java
@@ -1,3 +1,23 @@
+/*
+ * if문의 조건문과 다르게 case를 사용 → case애 사용할 수 있는 것 : 정수 , 문자 , 문자열
+ * → 값을 1개 지정
+ * → 값이 입력 → switch에 값을 받는다
+ * 	switch(op)
+ * → case 처음부터 검색
+ * → 해당문자,정수를 찾아서 문장을 수행
+ * → case를 찾아서 처리하고 break가 없는 경우에는 break가 존재하는 곳까지 문장을 수행
+ * ** 찾은 case부터break가 있는 case까지 수행
+ * ** 해당 case에 없는 경우에는 default(생략이 가능) → case마다 break;
+ * ** break는 switch~case를 종료
+ * 
+ * ** 보통 프로그램에서는 if를 많이 사용한다
+ * 	  경우의 수가 많은 경우에는 switch를 주로 이용한다(게임,메뉴)
+ *   → switch가 if보다 간결하다 
+ *   → 네트워크
+ * ** if → 범위가 있는 경우
+ * ** 값 1개씩 여러개인 경우 → switch
+ */
+
 import java.util.Scanner;
 
 public class 연습문제_1 {
