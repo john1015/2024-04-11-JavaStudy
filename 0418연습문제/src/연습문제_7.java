dd7585313669c8b7a71dd4a6d405699a6da24e64
정유엽 <39753330+john1015@users.noreply.github.com>
2024-04-18 17:48:19 +0900

    0418

--- 0418연습문제/src/연습문제_7.java
+++ 0418연습문제/src/연습문제_7.java
@@ -4,7 +4,7 @@ public class 연습문제_7 {
 	public static void main(String[] args) {
 		int cnt=0;
 		for (int i = 1; i <=30; i++) {
-			if(i%2==0) { System.out.print(i+" "); cnt++;}
+			if(i%2==0) { System.out.printf("%2d ",i); cnt++;}
 			if(cnt==3) { cnt=0; System.out.println();}
 		}
 		
