25eeffc8d0de1966dd304864315874f1af82513f
정유엽 <39753330+john1015@users.noreply.github.com>
2024-04-19 17:36:44 +0900

    0418

---
+++ 0418연습문제/src/연습문제_8.java
@@ -0,0 +1,13 @@
+
+public class 연습문제_8 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for (int i = 1; i <= 10; i++) {
+			if(i%2==0) sum-=i;
+			else sum+=i;
+		}
+		System.out.println("1-2+3..-10까지의합 : "+sum);
+	}
+
+}
