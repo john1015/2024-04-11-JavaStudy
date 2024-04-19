f30ef3b7305820e5940e0062d2477a3b9b1c8ff1
정유엽 <39753330+john1015@users.noreply.github.com>
2024-04-19 17:35:41 +0900

    0419

--- 0418연습문제/src/연습문제_8.java
+++ 0418연습문제/src/연습문제_8.java
@@ -1,13 +1,18 @@
+//8. 3이상 4462 이하에서 짝수인 정수의 합을 구하여라.
 
 public class 연습문제_8 {
 
 	public static void main(String[] args) {
-		int sum=0;
-		for (int i = 1; i <= 10; i++) {
-			if(i%2==0) sum-=i;
-			else sum+=i;
+		int jjak=0,hol=0;
+		for (int i = 3; i <= 4462; i++) {
+			if(i%2==0) jjak += i; 
+			else hol+=i;
 		}
-		System.out.println("1-2+3..-10까지의합 : "+sum);
+		System.out.println("3~4462짝수의 합 : "+jjak);
+		System.out.println("3~4462홀수의 합 : "+hol);
 	}
+	
+	
 
+	
 }
