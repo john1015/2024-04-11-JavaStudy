5e8fd4831ef68d3a059f4580dcdea67b2d66b430
정유엽 <39753330+john1015@users.noreply.github.com>
2024-04-19 17:42:04 +0900

    0419

--- Chapter01_03정리/src/연습문제_9.java
+++ Chapter01_03정리/src/연습문제_9.java
@@ -1,13 +1,21 @@
 //9. 0~12까지 2의 배수, 3의 배수의 합을 구하여라.
+// 1~100까지 7배수,9의배수의 합
 
 public class 연습문제_9 {
 
 	public static void main(String[] args) {
 		int sum=0;
+		int a7=0,a9=0;
 		for (int i = 0; i <= 12; i++) {
 			if(i%2==0||i%3==0||i%6==0) sum+=i;
 		}
 		System.out.println("2,3의배수의합 : "+sum);
+		for (int i = 1; i <=100; i++) {
+			if(i%7==0) a7+=i;
+			if(i%9==0) a9+=i;
+		}
+		System.out.println("7의 배수의합 : "+a7+" 9의 배수의 합 : "+a9);
+	
 	}
 
 }
