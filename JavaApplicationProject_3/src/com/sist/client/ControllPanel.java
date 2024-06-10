d9d76818cf4b5ea0e2f9aae31c31f7133503afec
정유엽 <39753330+john1015@users.noreply.github.com>
2024-06-10 17:50:56 +0900

    미니프로젝

--- JavaApplicationProject_3/src/com/sist/client/ControllPanel.java
+++ JavaApplicationProject_3/src/com/sist/client/ControllPanel.java
@@ -1,10 +1,15 @@
 package com.sist.client;
 
+import java.awt.CardLayout;
 import java.awt.Color;
 
 import javax.swing.JPanel;
 public class ControllPanel extends JPanel{
+	CardLayout card = new CardLayout();
+	EmpListPanel ep = new EmpListPanel();
 	public ControllPanel() {
-		setBackground(Color.cyan);
+		setLayout(card);
+		add("EP",ep);
 	}
+	
 }
